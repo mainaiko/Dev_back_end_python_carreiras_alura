288cacd419cfa6efa3c50e21e07d3a43041115e7
Natalia <mutantetrakor82@gmail.com>
2026-06-23 18:31:50 -0300

    propertys, inicioPOO, construtor e notes

---
+++ python_aplicando_POO/property_e_metodos/main.py
@@ -0,0 +1,26 @@
+class Restaurante:
+    restaurantes = []
+    
+    def __init__(self, nome, categoria): 
+        self.nome = nome 
+        self.categoria = categoria
+        self._ativo = False # definindo o atributo como privado para que o usuario nao o modifique
+        Restaurante.restaurantes.append(self) 
+    
+    def __str__(self): 
+        return f'{self.nome} - {self.categoria}'
+    
+    def listar_restaurantes(): 
+        print (f'{'Nome do restaurante.'.ljust(25)} | {'Categoria'.ljust(25)} | Ativo')
+        for restaurante in Restaurante.restaurantes:
+            print (f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property # usado para modificar a forma como o atributo vai ser acessado (lido)
+    def ativo(self):
+        return '☒' if self._ativo else '☐'
+    
+
+restaurante_praca = Restaurante('praça', 'churrascaria')
+restaurante_pizza = Restaurante('pizza place', 'fast food')
+
+Restaurante.listar_restaurantes()
